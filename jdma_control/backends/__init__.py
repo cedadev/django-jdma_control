2c8ec2ea36b4112f85c0a35c778f9687d9e50837
nmassey001 <neil.massey@stfc.ac.uk>
2018-03-21 10:19:48 +0000

    Added get_backend_ids and get_backend_from_id

--- jdma_control/backends/__init__.py
+++ jdma_control/backends/__init__.py
@@ -7,3 +7,10 @@ def get_backends():
     return [ElasticTapeBackend.ElasticTapeBackend,
             FakeElasticTapeBackend.FakeElasticTapeBackend,
             ObjectStoreBackend.ObjectStoreBackend]
+
+def get_backend_ids():
+    return [x.get_id(None) for x in get_backends()]
+
+def get_backend_from_id(id):
+    index = get_backend_ids().index(id)
+    return get_backends()[index]
