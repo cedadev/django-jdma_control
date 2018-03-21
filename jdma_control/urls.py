8695a37f5b6cbefa625cd531c87ea4a8ddc726e6
nmassey001 <neil.massey@stfc.ac.uk>
2018-03-21 10:25:47 +0000

    Added view and url to list files and archives

--- jdma_control/urls.py
+++ jdma_control/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from jdma_control.views import *
 
 urlpatterns = (
-    url(r'^api/v1/user', UserView.as_view()),
+    url(r'^api/v1/user/', UserView.as_view()),
     url(r'^api/v1/request', MigrationRequestView.as_view()),
-    url(r'^api/v1/migration', MigrationView.as_view()),
-    url(r'^api/v1/list_backends', list_backends, name="backends")
+    url(r'^api/v1/migration/', MigrationView.as_view()),
+    url(r'^api/v1/archive/', MigrationArchiveView.as_view()),
+    url(r'^api/v1/file/', MigrationFileView.as_view()),
+    url(r'^api/v1/list_backends/', list_backends, name="backends")
 )
