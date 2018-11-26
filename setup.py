7a1317c55e35171eb8deb3b5bfaf827d03a16288
nmassey001 <neil.massey@stfc.ac.uk>
2018-11-26 08:57:57 +0000

    Updated setup.py version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='jdma_control',
-    version='0.2.7',
+    version='0.2.8',
     packages=['jdma_control'],
     install_requires=[
         'appdirs',
