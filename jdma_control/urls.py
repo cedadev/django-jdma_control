7654cb517cb069cf85dbb9193fa3c4977a2160c2
Neil Massey <neil.massey@stfc.ac.uk>
2024-07-31 11:39:32 +0100

    Changed the path imports in views.py, upped the setup.py version

--- jdma_control/urls.py
+++ jdma_control/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import re_path
 from jdma_control.views import *
 
 urlpatterns = (
-    url(r'^api/v1/user', UserView.as_view()),
-    url(r'^api/v1/request', MigrationRequestView.as_view()),
-    url(r'^api/v1/migration/', MigrationView.as_view()),
-    url(r'^api/v1/archive/', MigrationArchiveView.as_view()),
-    url(r'^api/v1/file/', MigrationFileView.as_view()),
-    url(r'^api/v1/list_backends/', list_backends, name="backends")
+    re_path(r'^api/v1/user', UserView.as_view()),
+    re_path(r'^api/v1/request', MigrationRequestView.as_view()),
+    re_path(r'^api/v1/migration/', MigrationView.as_view()),
+    re_path(r'^api/v1/archive/', MigrationArchiveView.as_view()),
+    re_path(r'^api/v1/file/', MigrationFileView.as_view()),
+    re_path(r'^api/v1/list_backends/', list_backends, name="backends")
 )
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="jdma_control",
-    version="1.0.3",
+    version="1.0.4",
     packages=find_packages(),
     install_requires=[
         "appdirs",
