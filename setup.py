0a800bf0be0c9d9c92cef2541a057b205b8e2fef
Neil Massey <neil.massey@stfc.ac.uk>
2024-08-08 10:41:02 +0100

    Upped version to 1.0.7

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="jdma_control",
-    version="1.0.6",
+    version="1.0.7",
     packages=find_packages(),
     install_requires=[
         "appdirs",
